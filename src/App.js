6c97a917dc7deeb916286d64207bf78aea18c92b
boyhotkey96 <boyhotkey96@gmail.com>
2021-01-13 16:04:06 +0700

    add

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.css';
+import Home from './pages/home';
+import Detail from './pages/detail';
+import Register from './pages/register';
+
+function App() {
+  return <Home />
+}
+
+export default App;
